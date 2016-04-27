58b942b5f17b8305cb0136f1d8fdd9a2b779dfbb
harpribot <harshalpriyadarshi6@gmail.com>
2016-04-26 20:50:31 -0500

    Debug: Using lower memory to prevent fault

--- train_script_lstm_no_attn.py
+++ train_script_lstm_no_attn.py
@@ -7,7 +7,7 @@ review_summary_file = 'extracted_data/review_summary.csv'
 # Do using LSTM cell - without attention mechanism
 out_file = 'result/test_results_lstm_absence_attention.csv'
 lstm_net = lstm.NeuralNet(review_summary_file)
-lstm_net.set_parameters(batch_size=5, memory_dim=15,learning_rate=0.05)
+lstm_net.set_parameters(batch_size=5, memory_dim=1,learning_rate=0.05)
 lstm_net.begin_session()
 lstm_net.form_model_graph()
 lstm_net.fit()
