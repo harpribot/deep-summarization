e1643e3f7252024eef4ab9e75dc7983557b0d0f2
harpribot <harshalpriyadarshi6@gmail.com>
2017-01-13 16:15:55 +0530

    update condor scripts

--- condor_seed.sh
+++ condor_seed.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Simple - No Attention - GRU (for seeding)
-condorify_gpu_email python train_script_gru_simple_no_attn.py condor_logs/gru_simple_no_attn.txt
+condorify_gpu_email python train_scripts/train_script_gru_simple_no_attn.py condor_logs/gru_simple_no_attn.txt
